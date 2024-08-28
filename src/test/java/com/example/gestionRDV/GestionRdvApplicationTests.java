8b9b5a6abdf251b1f780908103b8f6f37b2874a4
Fatima-Zahra BOUDAD <fatimazahraboudad9@gmail.com>
2024-08-28 12:07:38 +0100

    initial commit

---
+++ src/test/java/com/example/gestionRDV/GestionRdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionRDV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
